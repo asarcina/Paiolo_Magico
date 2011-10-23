18dc0138342f5de328f55e6108d4f2af7dff21f6
Alessandro Sarcina <a.sarcina@gmail.com>
2011-10-23 13:42:27 +0200

    first commit

--- actions.rb
+++ actions.rb
@@ -1,24 +1,24 @@
-require 'rubygems'  
+require 'rubygems'
 
-class Actions 
-    
-    def initialize
+
+
+class Actions
+  def initialize
+  end
+
+  def insert_all_from_file (path)
+
+    file=File.open(path, 'r')
+    file.each_line do |line|
+      new_extraction = Extraction.new
+      new_extraction.set_from_input(line)
+      new_extraction.save
+
     end
-    
-    def insert_all_from_file (path)
-      
-      file=File.open(path, 'r')
-      file.each_line do |line|
-          new_extraction = Extraction.new
-          new_extraction.set_from_input(line)
-          new_extraction.save   
-      
-      end
-      rescue IOError => err
-        puts "i/o Error : #{err}"
-      ensure 
-        file.close
+  rescue IOError => err
+    puts "i/o Error : #{err}"
+  ensure
+    file.close
     end
-    
-  
+
 end
